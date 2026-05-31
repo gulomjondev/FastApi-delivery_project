9275dfbca6b54c6d0a90c7c3fbcbf39776c4500d
gulomjondev <gulomjon29102003@gmail.com>
2026-05-31 23:39:33 +0500

    Fix ChoiceType issue for Render

--- models.py
+++ models.py
@@ -1,15 +1,6 @@
 from database import Base
 from sqlalchemy import Column, String, Integer, Boolean, Text, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy_utils.types import ChoiceType
-
-
-ORDER_STATUS = [
-    ("PENDING", "Pending"),
-    ("IN_TRANSIT", "In Transit"),
-    ("DELIVERED", "Delivered"),
-    ("CANCELLED", "Cancelled"),
-]
 
 
 class User(Base):
@@ -21,7 +12,7 @@ class User(Base):
     is_staff = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
 
-    orders = relationship('Order', back_populates='user')  
+    orders = relationship('Order', back_populates='user')
 
     def __repr__(self):
         return f"{self.username}"
@@ -33,7 +24,7 @@ class Product(Base):
     name = Column(String(70))
     price = Column(Integer)
 
-    orders = relationship('Order', back_populates='product') 
+    orders = relationship('Order', back_populates='product')
 
     def __repr__(self):
         return f"{self.name} product {self.price}"
@@ -43,13 +34,13 @@ class Order(Base):
     __tablename__ = 'order'
     id = Column(Integer, primary_key=True)
     quantity = Column(Integer, nullable=False)
-    order_status = Column(ChoiceType(choices=ORDER_STATUS), default="PENDING")
+    order_status = Column(String(20), default="PENDING")
 
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship('User', back_populates='orders') 
+    user = relationship('User', back_populates='orders')
 
     product_id = Column(Integer, ForeignKey('product.id'))
-    product = relationship('Product', back_populates='orders') 
+    product = relationship('Product', back_populates='orders')
 
     def __repr__(self):
         return f"Order {self.id} {self.order_status}"
